180383a671085a869e656543ced24bab35da0b25
Alejandro Cabarcos <58979886+cabarcos19@users.noreply.github.com>
2021-03-03 19:04:43 +0100

    center_car() function and regions modification

--- src/self_driving.py
+++ src/self_driving.py
@@ -10,10 +10,14 @@ import math
 
 pub_ = None
 regions_ = {
-    'right': 0,
-    'fright': 0,
+    'back_right': 0,
+    'lateral_right':0,
+	'middle_right':0,
+	'fright': 0,
     'front': 0,
     'fleft': 0,
+	'middle_left': 0,
+	'lateral_left': 0,
     'left': 0,
 }
 state_ = 0
@@ -30,21 +34,38 @@ state_dict_ = {
 def clbk_laser(msg):
     global regions_
     regions_ = {
-        'right':  min(min(msg.ranges[0:109]), 10),
-        'fright': min(min(msg.ranges[110:174]), 10),
-        'front':  min(min(msg.ranges[175:184]), 10),
-        'fleft':  min(min(msg.ranges[185:249]), 10),
-        'left':   min(min(msg.ranges[250:359]), 10),
+        'back_right':  min(min(msg.ranges[0:84]), 12),
+		'lateral_right': min(min(msg.ranges[85:94]),12),
+        'middle_right': min(min(msg.ranges[95:109]),12),
+		'fright': min(min(msg.ranges[110:174]), 12),
+        'front':  min(min(msg.ranges[175:184]), 12),
+        'fleft':  min(min(msg.ranges[185:249]), 12),
+		'middle_left': min(min(msg.ranges[250:264]),12),
+        'lateral_left': min(min(msg.ranges[265:274]),12),
+		'back_left':   min(min(msg.ranges[275:359]), 12),
     }
-
+	
     take_action()
 
 
 def change_state(state):
     global state_, state_dict_
     if state is not state_:
-        print 'Wall follower - [%s] - %s' % (state, state_dict_[state])
+        print 'Action - [%s] - %s' % (state, state_dict_[state])
         state_ = state
+
+def center_car(float lateral_left_laser,float lateral_right_laser):
+	l_left = round(lateral_left_laser,2)
+	l_right = round(lateral_right_laser,2)
+	center = l_left + l_right
+	center_r = round(center,2)
+	
+	if(l_left < center_r):
+		while(l_left != center_r):
+			turn_fleft()
+	else:
+		while(l_right != center_r):
+			turn_fright()
 
 
 def take_action():
@@ -59,7 +80,9 @@ def take_action():
     d_detection = 1.5
     d = 1
     d_emergency = 0.1
-
+	
+	center_car(regions['lateral_left'],regions['lateral_right'])
+	
     if regions['front'] >= d_detection:
         state_description = 'case 0 - go straight'
         change_state(0)
@@ -91,13 +114,12 @@ def take_action():
         elif regions['fleft'] > d and regions['fright'] < d:
             state_description = 'case 1 - go front left'
             change_state(1)
+		#emergency stop
+		else:
+        	state_description = 'case 5 - emergency stop'
+	    	change_state(5)
 
-    #emergency stop        
-    if regions['front'] < d_emergency and regions['fleft'] < d_emergency and regions['fright'] < d_emergency:
-  	    state_description = 'case 5 - emergency stop'
-	    change_state(5)
-
-    rospy.loginfo(regions)
+    #rospy.loginfo(regions)
    
 
 def turn_fright():
@@ -150,8 +172,8 @@ def main():
     global pub_
 
     rospy.init_node('reading_laser')
-
-    pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+	#queue size = 1 by default
+    pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
 
     sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
 
@@ -165,12 +187,12 @@ def main():
         elif state_ == 2:
             msg = turn_fright()
         elif state_ == 3:
-	    msg = turn_right()
-	elif state_ == 4:
-	    msg = turn_left()
-	elif state_ == 5:
-	    msg = emergency_stop()
-	    pass
+	    	msg = turn_right()
+		elif state_ == 4:
+	    	msg = turn_left()
+		elif state_ == 5:
+	    	msg = emergency_stop()
+	    	pass
         else:
             rospy.logerr('Unknown state!')
 
